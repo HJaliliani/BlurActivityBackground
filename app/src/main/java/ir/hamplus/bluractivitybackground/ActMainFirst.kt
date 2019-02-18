66ca7f9cc0174e1a19e8f12213050b6102ebac3b
Hamed Jaliliani <jalilian_ham@yahoo.com>
2019-02-18 11:50:51 +0330

    First Build

---
+++ app/src/main/java/ir/hamplus/bluractivitybackground/ActMainFirst.kt
@@ -0,0 +1,12 @@
+package ir.hamplus.bluractivitybackground
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActMainFirst : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ly_act_first)
+    }
+}
